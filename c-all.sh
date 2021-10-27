e86dca668181e2c0e9161635652aa63ab4430065
vaderus <92164449+vaderus@users.noreply.github.com>
2021-10-27 18:59:45 +0000

    Add files via upload

---
+++ c-all.sh
@@ -0,0 +1,5 @@
+sed -i '/SchemeUrg/d' /home/gabriel/.cache/wal/colors-wal-dwm.h
+make clean install -C /home/gabriel/.sources/dwm
+make clean install -C /home/gabriel/.sources/slstatus
+make clean install -C /home/gabriel/.sources/dmenu
+make clean install -C /home/gabriel/.sources/st
